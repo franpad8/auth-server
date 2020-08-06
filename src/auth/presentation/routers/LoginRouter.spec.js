becf7a204f7a2d263ededb61828746b8ca7e94bb
franpad8 <franpad8@gmail.com>
2020-08-06 15:45:01 -0400

    Ensured LoginRouter returns 400 if not email is provided. Created MissingParamError.

--- src/auth/presentation/routers/LoginRouter.spec.js
+++ src/auth/presentation/routers/LoginRouter.spec.js
@@ -1,7 +1,21 @@
+class MissingParamError extends Error {
+  constructor (param) {
+    super(`Missing Param: ${param}`)
+    this.name = 'MissingParamError'
+  }
+}
+
 class HttpResponse {
   static serverError () {
     return {
       statusCode: 500
+    }
+  }
+
+  static badRequest (param) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(param)
     }
   }
 }
@@ -10,6 +24,10 @@ class LoginRouter {
   route (httpRequest) {
     if (!httpRequest || !httpRequest.body) {
       return HttpResponse.serverError()
+    }
+    const { email } = httpRequest.body
+    if (!email) {
+      return HttpResponse.badRequest('email')
     }
   }
 }
@@ -30,5 +48,17 @@ describe('LoginRouter', function () {
     const sut = makeSut()
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toEqual(500)
+  })
+
+  it('should return 400 if not email is provided', function () {
+    const httpRequest = {
+      body: {
+        password: 'letmein'
+      }
+    }
+    const sut = makeSut()
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toEqual(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
