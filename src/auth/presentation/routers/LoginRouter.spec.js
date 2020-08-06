adf92c0f24195619b2ad102432601d191681e74d
franpad8 <franpad8@gmail.com>
2020-08-06 15:22:52 -0400

    Ensured LoginRouter returns 400 if nor request object is provided

--- src/auth/presentation/routers/LoginRouter.spec.js
+++ src/auth/presentation/routers/LoginRouter.spec.js
@@ -1,6 +1,21 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+const makeSut = function () {
+  return new LoginRouter()
+}
 
 describe('LoginRouter', function () {
-  it('should return 400 if not httpRequest is passed', function () {
-    expect(true).toBe(true)
+  it('should return 400 if not httpRequest object is provided', function () {
+    const sut = makeSut()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toEqual(400)
   })
 })
